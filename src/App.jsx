e489a2470ab611543ca0729aa7a9f8a0cecae5ec
Otilia Toma <otilia@192-168-0-132.rdsnet.ro>
2024-01-06 23:19:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+import React from "react"
+import Header from "./components/Header"
+
+
+export default function App() {
+  return (
+    <div>
+    <Header />
+    <h1>Hello world!</h1>
+    </div>
+  )
+}
